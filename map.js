42ded5ba4bc8dcdd461827a934117fcc7c6e7938
carlosalfarodev <rgkalin@gmail.com>
2021-11-01 14:58:22 -0700

    map

---
+++ map.js
@@ -0,0 +1,8 @@
+console.log('map');
+
+const originalNumbers = [1, 4, 9, 16];
+
+// pass a function to map
+const numbersMultipliedByTwo = originalNumbers.map((item) => item * 2);
+
+console.log(numbersMultipliedByTwo);
